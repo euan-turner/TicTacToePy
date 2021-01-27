5ccfeca0b3f05b63f0054b46da67193fca52c112
Euan Turner <euanoturner@gmail.com>
2021-01-27 21:35:31 +0000

    Filled out some functions

--- v2/Player.py
+++ v2/Player.py
@@ -4,6 +4,7 @@ import pygame
 ##Token is a pre-drawn surface with a nought or cross
 ##val is 1 or -1, it also doubles as side from v1
 
+##Need a bool player.winner? for use in game.update_scores()
 @dataclass
 class Player:
     name : str
@@ -11,6 +12,7 @@ class Player:
     val : int
     text_colour : (int,int,int)
     score : int = 0
+    is_winner : bool = False
 
     ##Increment the score after a win
     def inc_score(self):
